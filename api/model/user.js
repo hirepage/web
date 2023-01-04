eebe006cab57103660e3c4993c8ac3e0b9fbed33
dgwight <dylan@otechie.com>
2023-01-03 21:10:20 -0300

    feat: vercel.json

---
+++ api/model/user.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+const autopopulate = require('mongoose-autopopulate')
+const timestamp = require('mongoose-timestamp')
+
+class UserClass {
+  get fullName () {
+    return this.lastName ? `${this.firstName} ${this.lastName}` : `${this.firstName}`
+  }
+}
+
+const schema = mongoose.Schema({
+  object: { type: String, default: 'User', required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, default: '' },
+  avatarUrl: { type: String, required: true },
+  email: { type: String, required: true },
+  bio: { type: String, }
+}, { toJSON: { virtuals: true } })
+
+schema.plugin(autopopulate)
+schema.plugin(timestamp)
+schema.loadClass(UserClass)
+module.exports = mongoose.model('User', schema)
